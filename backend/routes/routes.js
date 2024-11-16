09e875e511fc32e013b2f12d7b16df32f2c80bb6
Ayush Ranjan <ayusranjan@gmail.com>
2024-11-16 11:50:53 +0530

    add routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,26 @@
 import express from 'express';
 
+import { getAllForms, getFormById, updateIPStatus, submitFormDB } from '../controllers/IPValuation.js';
+import { postLoginFlow, onboardUser } from '../controllers/Authentication.js';
+import { getDueDiligence } from '../controllers/DueDiligence.js'
+import { getMyIPs } from '../controllers/IPTokenization.js';
+
 const router = express.Router();
+
+// Signup & Onboarding
+router.post('/postLoginFlow', postLoginFlow);
+
+// Signup & Onboarding
+router.post('/onboardUser', onboardUser);
+
+// IP Valuation
+router.get('/ipValuation/getAllForms', getAllForms);
+router.get('/ipValuation/getFormById', getFormById);
+router.post('/ipValuation/submitForm', submitFormDB);
+router.post('/ipValuation/updateIPStatus', updateIPStatus);
+router.get('/ipValuation/getDueDiligence', getDueDiligence);
+
+// IP Tokenization
+router.get('/ipTokenization/getMyIPs', getMyIPs);
 
 export default router;
